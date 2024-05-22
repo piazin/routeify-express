07ec09ecd1ed7097bc8dc752f1aee5450c4c4d3b
Lucas <ls4803326@gmail.com>
2024-05-21 23:51:08 -0300

    :sparkles: feat: added logger util to use

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./enums";
 export * from "./decorators";
 export * from "./exceptions";
+export * from "./utils/logger";
 export * from "./lib/createExpressServer";
 export { IExpressOptions, Request, Response, NextFunction } from "./types";
 export { createExpressServer as default } from "./lib/createExpressServer";
